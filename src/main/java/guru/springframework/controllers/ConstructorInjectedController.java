4801364dfa618051bfa29866ff576880e1bd97a5
Wojtek <wojtek19962a32@gmail.com>
2018-01-31 19:08:30 +0100

    Solution to the assignment

---
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -0,0 +1,24 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by jt on 5/24/17.
+ */
+@Controller
+public class ConstructorInjectedController {
+
+    private GreetingService greetingService;
+
+    @Autowired
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/main/java/guru/springframework/services/GreetingRepositoryImpl.java
@@ -0,0 +1,28 @@
+package guru.springframework.services;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jt on 5/24/17.
+ */
+@Service
+public class GreetingRepositoryImpl implements GreetingRepository {
+
+    @Override
+    public String getEnglishGreeting() {
+        return "Hello - Primary Greeting service";
+    }
+
+    @Override
+    public String getSpanishGreeting() {
+        return "Servicio de Saludo Primario";
+    }
+
+    @Override
+    public String getGermanGreeting() {
+
+        return "Primärer Grußdienst";
+
+    }
+}
